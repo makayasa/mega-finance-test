4220b6c93bdae88b330002f073109931f75120d3
Makayasa <rifqimakayasa@gmail.com>
2024-02-06 18:01:37 +0700

    network settings

---
+++ lib/app/controllers/network_controller.dart
@@ -0,0 +1,91 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:get/state_manager.dart';
+import 'package:get_storage/get_storage.dart';
+
+class NetworkController extends GetxController {
+  final box = GetStorage();
+  final Dio _dio = Dio();
+
+  CookieJar cookieJar = CookieJar();
+
+  Future<({bool isConnected, ConnectivityResult connectedVia})> checkConnection() async {
+    _dio.options.connectTimeout = const Duration(milliseconds: 5000);
+    final res = await Connectivity().checkConnectivity();
+    if (res == ConnectivityResult.none) {
+      return (isConnected: true, connectedVia: ConnectivityResult.none);
+    }
+    return (isConnected: true, connectedVia: res);
+  }
+
+  Future<dynamic> get(
+    String path, {
+    Map<String, dynamic>? query,
+    Map<String, dynamic>? body,
+    bool isTest = false,
+    String cookies = '',
+  }) async {
+    bool isConnect = false;
+    if (isTest) {
+      isConnect = true;
+    } else {
+      final res = await checkConnection();
+      isConnect = res.isConnected;
+    }
+    if (!isConnect) {
+      return;
+    }
+    try {
+      // final a = GetStorage().read('cookies');
+      Response res = await _dio.get(
+        path,
+        queryParameters: query,
+        data: body,
+        options: Options(
+          headers: {"Cookie": cookies},
+          contentType: 'application/json',
+        ),
+      );
+      return res;
+    } on DioException catch (e) {
+      // ErrorHelper().errorCode(e.response.statusCode);
+      rethrow;
+    }
+  }
+
+  Future<dynamic> post(
+    String path, {
+    Map<String, dynamic>? query,
+    Map<String, dynamic>? body,
+    bool isTest = false,
+  }) async {
+    bool isConnect = false;
+    if (isTest) {
+      isConnect = true;
+    } else {
+      final res = await checkConnection();
+      isConnect = res.isConnected;
+    }
+    if (!isConnect) {
+      return;
+    }
+    try {
+      Response res = await _dio.post(
+        path,
+        queryParameters: query,
+        data: body,
+      );
+      return res;
+    } on DioException {
+      rethrow;
+    }
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    _dio.interceptors.add(CookieManager(cookieJar));
+  }
+}
